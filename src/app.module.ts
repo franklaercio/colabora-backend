d5bc0eb247ee6809af0c8d9bdd78f60e19f729fb
Frank <franklaercio@gmail.com>
2023-06-15 22:30:50 -0300

    feat: adding suggestions and categories modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { SuggestionService } from './services/suggestion/suggestion.service';
+import { SuggestionController } from './controllers/suggestion/suggestion.controller';
+import { SuggestionModule } from './modules/suggestion/suggestion.module';
+import { CategoryService } from './services/category/category.service';
+import { CategoryModule } from './modules/category/category.module';
+import { CategoryController } from './controllers/category/category.controller';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     UserModule,
-    MongooseModule.forRoot(process.env.MONGODB_URL)
+    MongooseModule.forRoot(process.env.MONGODB_URL),
+    SuggestionModule,
+    CategoryModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, SuggestionController, CategoryController],
+  providers: [AppService, SuggestionService, CategoryService],
 })
 export class AppModule {}
